74aa4f5a6bb745ad380dfb74d721eafdd7d9b3d9
814151789@qq.com <814151789@qq.com>
2025-01-13 21:11:48 +0800

    add house

--- scenes/characters/player/tilling_state.gd
+++ scenes/characters/player/tilling_state.gd
@@ -3,6 +3,7 @@ extends NodeState
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
+
 func _on_process(_delta : float) -> void:
 	pass
 
@@ -12,23 +13,21 @@ func _on_physics_process(_delta : float) -> void:
 
 
 func _on_next_transitions() -> void:
-	print(animated_sprite_2d.is_playing())
 	if !animated_sprite_2d.is_playing():
 		transition.emit('Idle')
-		
 
 
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
-		animated_sprite_2d.play('chopping_back')
-	elif player.player_direction == Vector2.DOWN:
-		animated_sprite_2d.play('chopping_front')
-	elif player.player_direction == Vector2.LEFT:
-		animated_sprite_2d.play('chopping_left')
+		animated_sprite_2d.play('tilling_back')
 	elif player.player_direction == Vector2.RIGHT:
-		animated_sprite_2d.play('chopping_right')
+		animated_sprite_2d.play('tilling_right')
+	elif player.player_direction == Vector2.DOWN:
+		animated_sprite_2d.play('tilling_front')
+	elif player.player_direction == Vector2.LEFT:
+		animated_sprite_2d.play('tilling_left')
 	else:
-		animated_sprite_2d.play('chopping_front')
+		animated_sprite_2d.play('tilling_front')
 
 
 func _on_exit() -> void:
--- scenes/characters/player/watering_state.gd
+++ scenes/characters/player/watering_state.gd
@@ -12,23 +12,21 @@ func _on_physics_process(_delta : float) -> void:
 
 
 func _on_next_transitions() -> void:
-	print(animated_sprite_2d.is_playing())
 	if !animated_sprite_2d.is_playing():
 		transition.emit('Idle')
-		
 
 
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
-		animated_sprite_2d.play('chopping_back')
-	elif player.player_direction == Vector2.DOWN:
-		animated_sprite_2d.play('chopping_front')
-	elif player.player_direction == Vector2.LEFT:
-		animated_sprite_2d.play('chopping_left')
+		animated_sprite_2d.play('watering_back')
 	elif player.player_direction == Vector2.RIGHT:
-		animated_sprite_2d.play('chopping_right')
+		animated_sprite_2d.play('watering_right')
+	elif player.player_direction == Vector2.DOWN:
+		animated_sprite_2d.play('watering_front')
+	elif player.player_direction == Vector2.LEFT:
+		animated_sprite_2d.play('watering_left')
 	else:
-		animated_sprite_2d.play('chopping_front')
+		animated_sprite_2d.play('watering_front')
 
 
 func _on_exit() -> void:
